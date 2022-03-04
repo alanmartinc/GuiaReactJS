fa87089cb12fc6dab44239d11e18e5cde3a21a6d
alanmartinc <alancabot.ac@gmail.com>
2022-03-04 13:01:39 -0300

    Analizando useState

--- login-app/src/App.js
+++ login-app/src/App.js
@@ -1,36 +1,16 @@
 import {useState} from "react";
 
 function App() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [name, setName] = useState("Alan");
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    alert(`Usuario: ${email}, Password: ${password}`);
-  };
-
-  // Destructuring
-  const handlePasswordChange = ({target: {value}}) => setPassword(value);
-  const handleEmailChange = ({target: {value}}) => setEmail(value);
+  const handleHeyClick = () => {
+    setName("Martin");
+  }
 
   return (
     <div>
-      <form onSubmit={handleFormSubmit}>
-        <h2>Iniciar sesión</h2>
-        <label>
-          Correo
-          <input type="email" value={email} onChange={handleEmailChange} />
-        </label>
-
-        <label>
-          Contraseña
-          <input type="password" value={password} onChange={handlePasswordChange} />
-        </label>
-
-        <button type="submit">
-          Enter
-        </button>
-      </form>
+      <h2>Hola {name}</h2>
+      <button onClick={handleHeyClick}>Hey</button>
     </div>
   );
 }
