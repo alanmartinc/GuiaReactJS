2950905d717c4fd4466b94e9643011266138fa25
alanmartinc <alancabot.ac@gmail.com>
2022-03-07 16:20:03 -0300

    Ciclo de vida de un componente

--- login-app/src/App.js
+++ login-app/src/App.js
@@ -1,6 +1,6 @@
-import {useState} from "react";
+import {useState, Component} from "react";
 
-function App() {
+/* function App() {
   const [name, setName] = useState("Alan");
 
   const handleHeyClick = () => {
@@ -13,6 +13,47 @@ function App() {
       <button onClick={handleHeyClick}>Hey</button>
     </div>
   );
+} */
+
+class App extends Component {
+  // Se utiliza para declarar variables de estado
+  constructor(props) {
+    super(props);
+    console.log("constructor");
+    
+    // Se declaran las props
+    this.state = {
+      name: "",
+    }
+  }
+
+  // Se utiliza para hacer llamadas a la API
+  componentDidMount() {
+    console.log("Did Mount");
+  }
+
+  // Se utiliza para actualizar un nuevo estado
+  componentDidUpdate() {
+    console.log("Did Update");
+  }
+
+  // Previene renderings innecesarios
+  shouldComponentUpdate() {
+    return true;
+  }
+
+  // Se utiliza para ver el estado y propiedades previas antes de ser actualizadas
+  getSnapshotBeforeUpdate(preProps, prevState) {
+    console.log(preProps, prevState);
+  }
+
+  render() {
+    return (
+      <div>
+        <input value={this.state.name} onChange={({target: {value}}) => this.setState({name: value})} />
+      </div>
+    );
+  }
 }
 
 export default App;
