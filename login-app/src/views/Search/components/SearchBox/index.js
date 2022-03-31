80697abd67f2cc8e8d84dffd4da8d5c6823806cb
alanmartinc <alancabot.ac@gmail.com>
2022-03-30 23:55:52 -0300

    Se agrega estilos

--- login-app/src/views/Search/components/SearchBox/index.js
+++ login-app/src/views/Search/components/SearchBox/index.js
@@ -1,16 +1,24 @@
 import { useState } from 'react';
+import "./style.css";
 
 export default function SearchBox() {
     const [searchText, setSearchText] = useState("");
     
     return(
-        <div>
-            <div>
-                Buscador de personal
+        <div className="search-box">
+            <h2 className="search-box-title">Buscador de personal</h2>
+            
+            <div className='search-box-buttons'>
                 <label>
-                    <input value={searchText} onChange={({target: {value}}) => setSearchText(value)} />
+                    <input 
+                        value={searchText} 
+                        onChange={({target: {value}}) => setSearchText(value)}
+                        className="search-box-input" 
+                    />
                 </label>
+
                 <button>Buscar</button>
+                <button>Cerrar</button>
             </div>
         </div>
     )
--- login-app/src/views/Search/index.js
+++ login-app/src/views/Search/index.js
@@ -1,8 +1,10 @@
 import SearchBox from "./components/SearchBox";
 
+import "./style.css";
+
 export default function Search() {
     return(
-        <div>
+        <div className="search">
             <SearchBox />
         </div>
     );
